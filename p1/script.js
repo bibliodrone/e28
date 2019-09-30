0dafb7a6598757a50e0fe26fdb631af787b4ce5e
Gerry Walden <gerald.walden@gmail.com>
2019-09-29 20:45:30 -0400

    adapting js to Vue format

--- p1/script.js
+++ p1/script.js
@@ -1,41 +1,50 @@
-let lever = document.getElementById("lever");
+var app = new Vue({
+  el: '#app', 
 
-lever.addEventListener("click", slotMachine);
-
-function slotMachine() {
-  let jackpot = 0; // will remain 0 until two or more slots yield the same symbol
-
-  const items = ["🍎", "➆", "♠", "🍒", "♥", "🍌"];
-
-  let slots = document.getElementsByClassName("slot");
-  let results = [];
-
-  for (var n = 0; n < slots.length; n++) {
-    let randomNumber = Math.floor(Math.random() * (items.length));
-    slots[n].innerText = items[randomNumber];
-    results.push(items[randomNumber])
-  }
-
-  if (results[0] == results[1]) { // if one and two match
-    if (results[1] == results[2]) { // then if two and three also match
-      jackpot = 2; // all three match ... jackpot!
-    } else {
-      jackpot = 1; // only 2 of 3 match -- partial jackpot
+  methods: {
+    slotMachine() {
+      let jackpot = 0; // will remain 0 until two or more slots yield the same symbol
+  
+      const items = ["🍎", "➆", "♠", "🍒", "♥", "🍌"];
+  
+      let slots = document.getElementsByClassName("slot");
+      let results = [];
+  
+      for (var n = 0; n < slots.length; n++) {
+        let randomNumber = Math.floor(Math.random() * (items.length));
+        slots[n].innerText = items[randomNumber];
+        results.push(items[randomNumber])
+      }
+  
+      if (results[0] == results[1]) { // if one and two match
+        if (results[1] == results[2]) { // then if two and three also match
+          jackpot = 2; // all three match ... jackpot!
+        } else {
+          jackpot = 1; // only 2 of 3 match -- partial jackpot
+        }
+      } else {
+        if (results[1] == results[2] || results[0] == results[2]) { //if only two and three or one and three match
+          jackpot = 1; // only a partial 2 of 3 jackpot
+        }
+      }
+  
+      if (jackpot == 0) {
+        document.getElementById("output-message").textContent = "Better luck next time...";
+      }
+      if (jackpot == 1) {
+        document.getElementById("output-message").textContent = "Partial match...";
+      }
+      if (jackpot == 2) {
+        document.getElementById("output-message").textContent = "Jackpot!";
+      }
+  
     }
-  } else {
-    if (results[1] == results[2] || results[0] == results[2]) { //if only two and three or one and three match
-      jackpot = 1; // only a partial 2 of 3 jackpot
-    }
   }
+/*
+  let lever = document.getElementById("lever");
 
-  if (jackpot == 0) {
-    document.getElementById("output-message").textContent = "Better luck next time...";
-  }
-  if (jackpot == 1) {
-    document.getElementById("output-message").textContent = "Partial match...";
-  }
-  if (jackpot == 2) {
-    document.getElementById("output-message").textContent = "Jackpot!";
-  }
+  lever.addEventListener("click", slotMachine);
 
-}
+  
+  */
+});

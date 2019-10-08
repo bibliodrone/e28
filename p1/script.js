e42cf522f50dc065ba8010e080f6436c723940e3
Gerry Walden <gerald.walden@gmail.com>
2019-10-07 23:51:56 -0400

    Major updates implementing Vue principles:

--- p1/script.js
+++ p1/script.js
@@ -1,50 +1,128 @@
-var app = new Vue({
-  el: '#app', 
+let app = new Vue({
+  el: '#app',
+  data: {
+    items: ["🍎", "➆", "♠", "🍒", "♥", "🍌"],
+    jackpot: 0,
+    wager: 5.00,
+    wagerError: false,
+    balance: 500.00,
+    slot1:'?',
+    slot2:'?',
+    slot3:'?',
+    gameMode: "initial",
+    outcomeMsg: '',
+    busted: false,
+  },
+  computed: {
+    balanceDisplay: function () {
+      return this.balance.toFixed(2);
+    },
+    wagerDisplay: function() {
+      return this.wager.toFixed(2);
+    },
+    wonJackpot : function() {
+      return this.jackpot == 3;
+    }
+  }, 
+
+  watch: {
+    balance: function() {
+      if (this.balance < 1) {
+        this.playAgain();
+      }
+    }
+  },
 
   methods: {
-    slotMachine() {
-      let jackpot = 0; // will remain 0 until two or more slots yield the same symbol
-  
-      const items = ["🍎", "➆", "♠", "🍒", "♥", "🍌"];
-  
-      let slots = document.getElementsByClassName("slot");
-      let results = [];
-  
-      for (var n = 0; n < slots.length; n++) {
-        let randomNumber = Math.floor(Math.random() * (items.length));
-        slots[n].innerText = items[randomNumber];
-        results.push(items[randomNumber])
-      }
-  
-      if (results[0] == results[1]) { // if one and two match
-        if (results[1] == results[2]) { // then if two and three also match
-          jackpot = 2; // all three match ... jackpot!
-        } else {
-          jackpot = 1; // only 2 of 3 match -- partial jackpot
-        }
+    pullTheLever: function() {
+      if (this.wager > this.balance && this.balance > 0) {
+        this.wagerError = true;
+        this.wager = this.balance;
+      }  else if (this.balance < 1) {
+        this.playAgain();
       } else {
-        if (results[1] == results[2] || results[0] == results[2]) { //if only two and three or one and three match
-          jackpot = 1; // only a partial 2 of 3 jackpot
+        this.wagerError = false;
+        this.jackpot = 0;
+        this.gameMode = "play";
+        var slots = [];
+        for (var i = 0; i < 3; i++) {
+          let randomNumber = Math.floor(Math.random() * (this.items.length));
+          slots.push(randomNumber);
         }
-      }
+        this.slot1 = this.items[slots [0]];
+        this.slot2 = this.items[slots [1]];
+        this.slot3 = this.items[slots [2]];
+        //console.log(slots);
+        //this.balance -= this.wager;
   
-      if (jackpot == 0) {
-        document.getElementById("output-message").textContent = "Better luck next time...";
+        this.betOutcome(slots);
       }
-      if (jackpot == 1) {
-        document.getElementById("output-message").textContent = "Partial match...";
+    },
+
+    betOutcome: function(slots) {
+      if (this.balance < 1) {
+        this.playAgain();
+      } else {
+        var balanceChange;
+        var newBalance = this.balance;
+        var s1 = slots[0];
+        var s2 = slots[1];
+        var s3 = slots[2];
+
+        //console.log(s1 + " " + s2 + "  " + s3);
+        var s1s2 = (s1 == s2);
+        var s2s3 = (s2 == s3);
+        var s1s3 = (s1 == s3);
+        //console.log(s1s2, s2s3, s1s3);
+
+        if(s1s2) {this.jackpot += 1};
+        if(s2s3) {this.jackpot += 1};
+        if(s1s3) {this.jackpot += 1};
+
+        console.log("Jackpot status " + this.jackpot.toFixed(2));
+      
+        if (this.jackpot == 0) {
+          balanceChange = (-1 * this.wager);
+
+          this.outcomeMsg = "Loss: " + balanceChange.toFixed(2);
+
+          //document.getElementById("output-message").textContent = "Better luck next time...";
+        }
+        else if (this.jackpot == 1 || this.jackpot == 2) {
+          balanceChange = ((-1 * this.wager)/2);
+          this.outcomeMsg = "Loss: " +  balanceChange.toFixed(2);
+          //document.getElementById("output-message").textContent = "Partial match...";
+        }
+        else if (this.jackpot == 3) {
+          balanceChange = (1.5 * this.wager);
+          this.outcomeMsg = "Jackpot! You win " + (balanceChange.toFixed(2));
+          //document.getElementById("output-message").textContent = "Jackpot!";
+        } 
+        else {
+          this.outcomeMsg = "Jackpot code: " + jackpot;
+        }
+        newBalance = newBalance + balanceChange;
+        if(newBalance < 1) {
+          this.playAgain();
+        }
+        this.balance = newBalance;
       }
-      if (jackpot == 2) {
-        document.getElementById("output-message").textContent = "Jackpot!";
-      }
-  
+    },
+
+    playAgain : function () {
+      this.balance = 0;
+      this.wager = 0;
+      this.outcomeMessage = "Better Luck Next Time!";
+      this.gameMode = "end";
+    },
+
+
+    resetGame : function () {
+      this.balance = 500;
+      this.wager = 5;
+      this.wagerError = false;
+      this.outcomeMsg = '';
+      this.gameMode = "initial";
     }
   }
-/*
-  let lever = document.getElementById("lever");
-
-  lever.addEventListener("click", slotMachine);
-
-  
-  */
 });

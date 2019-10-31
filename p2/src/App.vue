544ae1ae89a9ee3890ccbb3bea2f97e2240c0532
Gerry Walden <gerald.walden@gmail.com>
2019-10-31 01:14:37 -0400

    mostly have logic working

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <header>
       <h2>Slot Machine</h2>
         <p>Enter a wager (default is $25.00)</p>
@@ -7,41 +7,96 @@
         <p>If you match two out of three slots, you keep half your wager.</p>
         <p>Jackpot wins 2.5 x wager.</p>
     </header>
-    <slot-machine>
-      </slot-machine>
-    <div id = "hinge">
-      <span>&#x2593;&#x2592;&#x2593;</span>
+    <div id = "resetMsg">
+      <button id = "reset" @click='resetGame'>Game Reset</button>
     </div>
-    <div id = "lever">
-      <button @click='pullTheLever'>Pull the Lever!</button>
+   
+    <slot-machine
+    :balanceDisplay=balanceDisplay
+    :wager=wager
+    :slot1=slot1
+    :slot2=slot2
+    :slot3=slot3
+    :outcomeMsg=outcomeMsg>
+    
+    </slot-machine>
+    <div id="leverDiv">
+      <button id='lever' @click='pullTheLever'>Pull the Lever!</button>
     </div>
   </div>
- </template>
+</template>
 
 <script>
 import SlotMachine from './components/SlotMachine.vue'
 
 export default {
-  data:function() {
-    return {};
-  },
+  name:"app",
   components: {
     SlotMachine
   },
-  props: {
-    items: {
-      type: Array, 
-      default: ["🍎", "➆", "♠", "🍒", "♥", "🍌"]
-    }
+
+  data:function() {
+    return {
+      slots: [],
+      items: ["🍎", "➆", "♠", "🍒", "♥", "🍌"],
+      jackpot: 0,
+      wager: 25.00,
+      wagerError: false,
+      balance: 500.00,
+      slot1:'?',
+      slot2:'?',
+      slot3:'?',
+      gameMode: "initial",
+      lostMoney: false,
+      outcomeMsg:"Give it a try!"
+    };
   },
 
+  computed: {
+    balanceDisplay: function () {
+      return this.balance.toFixed(2);
+    },
+    wagerDisplay: function() {
+      return this.wager.toFixed(2);
+    },
+    wonJackpot : function() {
+      return this.jackpot == 3; //Applies styling class to #outcome-message on Jackpot (jackpot: 3).
+    }
+  }, 
+  
   methods: {
-      /* Ascertain the outcome of the round, based on the degree of matching between the three slots
+    /* Validate input wager, set game-mode to play, generate random numbers to simulate
+       the place where each slot comes to rest. 
+    */
+    pullTheLever: function() {
+      if (this.wager > this.balance && this.balance > 0) {
+        this.wagerError = true;
+        this.wager = this.balance;
+      }  else if (this.balance < 1) {
+        this.playAgain();
+      } else {
+        this.wagerError = false;
+        this.jackpot = 0;
+        this.gameMode = "play";
+        var slots = [];
+        for (var i = 0; i < 3; i++) {
+          let randomNumber = Math.floor(Math.random() * (this.items.length));
+          slots.push(randomNumber);
+        }
+        this.slot1 = this.items[slots [0]];
+        this.slot2 = this.items[slots [1]];
+        this.slot3 = this.items[slots [2]];
+  
+        this.betOutcome(slots);
+      }
+    },
+
+    /* Ascertain the outcome of the round, based on the degree of matching between the three slots
        Jackpot result score is 0-3; 0 = no matching slots; 1 or 2 = partial match; 3 = Jackpot;
        It's a crude but straightforward way to do the test. 
        Jackpot score determines wager outcome: total wager loss, 0.5 wager loss, jackpot win = 2.5 * wager.
     */
-    betOutcome: function(slots) {   
+    betOutcome: function(slots) {
       if (this.balance < 1) {
         this.playAgain();
       } else {
@@ -80,7 +135,7 @@ export default {
         } 
         
         else {
-          this.outcomeMsg = "Jackpot code unknown";
+          this.outcomeMsg = "Jackpot code?";
         }
         
         newBalance = newBalance + balanceChange;
@@ -112,12 +167,37 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+header {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    margin-bottom: 40px;
+    margin-left: 25px;
+}
+
+header p {
+    margin-top: 8px;
+    margin-bottom: 8px; 
+}
+
+#resetMsg {
+  text-align:center;
+  padding:10px;
+  margin: 25px;
+  max-width: 120px;
+  border-radius: 5px;
+  background-color:lightgreen
+}
+
+#reset {
+  border-radius: 3px;
+  color: green;
+}
+
+#leverDiv {
+  margin-left: 320px;
+  margin-top: 10px;
+}
+#lever {
+  border-radius: 0px 3px 3px 0px;
+  font-size: 24px;
 }
 </style>
--- p2/src/components/SlotMachine.vue
+++ p2/src/components/SlotMachine.vue
@@ -1,128 +1,136 @@
 <template>
-    <div id="slots">
-      <hr>
-        <div id = "fundAndWager" class = "display">
-        <div id = "funds">Funds: {{ balanceDisplay }}</div>
-          <!-- initial:  -->
-          <div v-if = 'gameMode == "initial"'>
-            <div id = "wager">Wager: {{wager}}</div>
+        <div id = "slotMachine">
+            
+          <div id = "wagerPanel">
+            <label for ='wager'>Enter a wager: &nbsp;</label>
+            <input type='number' min = "5" pattern = "[0-9]{1:4}" step = "5" id = 'wager' v-model.number='wager' @keyup.enter='pullTheLever' autofocus>
+            
           </div>
-          <!-- play:     -->
-          <div v-if = 'gameMode=="play"'>
-            <div id = "wager" v-bind:class = "{error: wagerError}">Wager: {{ wagerDisplay }}</div>
-          </div>
-          <!-- end:      -->
-          <div v-if = 'gameMode == "end"'>
-            <div id = "wager" >Wager: XXX </div>
-          </div>
-        </div>
-        <div class="display" id = "slotsDisplay">
-           <div id="slot1" class = "slot">{{slot1}}</div>
-           <div id="slot2" class = "slot">{{slot2}}</div>
-           <div id="slot3" class = "slot">{{slot3}}</div>
-        </div>
-        <hr>
-        <div id = "background">
-      <div id = "wagerMsg" v-if = 'wagerError == true'>If wager exceeds funds, wager will be set to max remaining funds.</div>
-      <div id = "playAgain" v-if = 'gameMode == "end"'>
-        <label for = "reset">Game Over. Reset funds and play another round?</label>
-        <button id = "reset" @click='resetGame'>Yes!</button>
-      </div>
 
-      <div id = "slotMachine">
-        <div id = "wagerPanel">
-          <label for ='wager'>Enter a wager</label>
-          <input type='number' min = "5" pattern = "[0-9]{1:4}" step = "5" id = 'wager' v-model.number='wager' @keyup.enter='pullTheLever' autofocus>
-        </div>
-      </div>
-      <div id="bottom-panel">
-      <div id="output-message" v-bind:class = "{jackpotMsg:wonJackpot, error:lostMoney}">{{ outcomeMsg }}</div>
-    </div>
-      </div>
-    </div>
+          <div id = "fundAndWager">
+            <div id = "funds">Funds: {{ balanceDisplay }}</div>
+            <div id = "wager">Wager: {{wager}}</div>
+          
+            <div id = "slotsDisplay">
+              
+              <div id="slot1" class = "slot"> {{slot1}} </div>
+              <div id="slot2" class = "slot"> {{slot2}} </div>
+              <div id="slot3" class = "slot"> {{slot3}} </div>
+              
+            </div>
+          </div>
+
+          <hr>
+          <div id="bottom-panel">
+            <div id="output-message" v-bind:class = "{jackpotMsg:wonJackpot, error:lostMoney}">{{ outcomeMsg }}</div>
+          </div>
+          
+        </div> <!-- End of 'slot machine'-->
 </template>
 
 <script>
 export default {
-  data: function () {
-      return{};
+  data: function() {
+    return {};
   },
-  props: {
-    jackpot: {type:Number, default:0},
-    wager: {type:Number, default:25.00},
-    wagerError: {type:Boolean, default:false},
-    balance: {type:Number, default:500.00},
-    slot1: {type:String, default:'?'},
-    slot2: {type:String, default:'?'},
-    slot3: {type:String, default:'?'},
-    gameMode: {type:String, default:'initial'},
-    outcomeMsg: {type:String, default:''},
-    lostMoney: {type:Boolean, default:false},
-  },
-  computed: {
-    balanceDisplay: function () {
-      return this.balance.toFixed(2);
+
+  props:{
+    outcomeMsg: {
+      type:String,
+      default:"",
     },
-    wagerDisplay: function() {
-      return this.wager.toFixed(2);
+    balanceDisplay: {
+      type:Number,
+      default:0
     },
-    wonJackpot : function() {
-      return this.jackpot == 3; //Applies styling class to #outcome-message on Jackpot (jackpot: 3).
+    wagerDisplay: {
+      type:Number,
+      default:0,
+    },
+    wager: {
+      type:Number,
+      default:0,
+    },
+    wonJackpot: {
+      type:Boolean,
+      default:false,
+    }, 
+    lostMoney: {
+      type:Boolean,
+      default:false,
+    },
+    slot1: {
+      type: String,
+      default: " ",
+    },
+    slot2: {
+      type: String,
+      default: " "
+    },
+    slot3: {
+      type: String,
+      default: " "
     }
-  }, 
-  watch: {
-    balance: function() {
-      if (this.balance < 1) {
-        this.playAgain();
-      }
-    }
-  },
-  methods:{
-    /* Validate input wager, set game-mode to play, generate random numbers to simulate
-       the place where each slot comes to rest. 
-    */
-    pullTheLever: function() {
-      if (this.wager > this.balance && this.balance > 0) {
-        this.wagerError = true;
-        this.wager = this.balance;
-      }  else if (this.balance < 1) {
-        this.playAgain();
-      } else {
-        this.wagerError = false;
-        this.jackpot = 0;
-        this.gameMode = "play";
-        var slots = [];
-        for (var i = 0; i < 3; i++) {
-          let randomNumber = Math.floor(Math.random() * (this.items.length));
-          slots.push(randomNumber);
-        }
-        this.slot1 = this.items[slots [0]];
-        this.slot2 = this.items[slots [1]];
-        this.slot3 = this.items[slots [2]];
-  
-        this.betOutcome(slots);
-      }
-    },
   }
 }
 
-
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style>
-h3 {
-  margin: 40px 0 0;
+#wagerPanel {
+    margin: 10px;
+    border-radius: 10px;
+    padding: 15px;
+    background: lightgray;
+    display: inline-block;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#wagerPanel input {
+  font-size: 20px; 
+  font-family:'Courier New', Courier, monospace;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#fundAndWager {
+  background: white;
+  font-family:Verdana, Geneva, Tahoma, sans-serif;
+  line-height: 1.6em;
 }
-a {
-  color: #42b983;
+#slotMachine {
+    width: 600px;
+    margin-left: 100px;
+    border: 5px dotted black;
+    display: inline-block;
+    background-color: red;
+    border-radius: 10px 10px 0px 0px;
+    text-align: center;
 }
+.slot {
+    display:inline-block;
+    width:100px;
+    overflow: hidden;
+    background-color: yellow;
+    font-family: Consolas;
+    font-size: 48px;
+    text-align: center;
+    padding: 20px 20px;
+    margin: 0px 10px;
+    box-sizing: border-box;
+    border: 1px solid lightgray;
+}
+#slotsDisplay {
+    text-align:center;
+    background-color: whitesmoke;
+    border: 2px solid gray;
+    margin: 10px;
+    border-radius: 5px;
+
+}
+#output-message {
+    height:35px;
+    background:yellow;
+    font-size: 20px;
+    margin-top: 15px;
+    font-family: 'Courier New', Courier, monospace;
+}
+
 </style>
